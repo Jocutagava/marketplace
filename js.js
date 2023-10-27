4001f01a95860ab19778e1f7724f62c37e5365c9
Jocutagava <bratoliubkirill@gmail.com>
2023-10-27 15:53:07 +0200

    vjvj

--- js.js
+++ js.js
@@ -14,12 +14,57 @@
 //   })
    
 // })
-let link = 'https://github.com/Jocutagava'
+let main = document.getElementById("main")
+let link = 'https://my-json-server.typicode.com/Jocutagava/marketplace/db'
 let xhr = new XMLHttpRequest();
 xhr.open('GET', link)
 xhr.responseType = 'json';
-xhr.send();
-xhr.onload = function(){
-   console.log(xhr.responce)
-}
 
+xhr.onload = function(){
+
+
+
+   draw(xhr.response)
+}
+xhr.send();
+
+
+
+function draw (a){
+    main.innerHTML = 
+    `
+    <div id="product1">
+    <h2>${a.products[0].name}</h2>
+    <img src="${a.products[0].photo_url}" alt="">
+    <p><b>Price</b>:${a.products[0].price}</p>
+    <p><b>Description</b>:${a.products[0].description}</p>
+    <br>
+    <a href="">Seller profile</a>
+    <br>
+    <button id="but"><b>Buy</b></button>
+    </div>
+
+    <div id="product2"> 
+    <h2>${a.products[1].name}</h2>
+    <img src="${a.products[1].photo_url}" alt="">
+    <p><b>Price</b>:${a.products[1].price}</p>
+    <p><b>Description</b>:${a.products[1].description}</p>
+    <br>
+    <a href="">Seller profile</a>
+    <br>
+    <button id="but"><b>Buy</b></button>
+    </div>
+
+    <div id="product3">
+    <h2>${a.products[2].name}</h2>
+    <img src="${a.products[2].photo_url}" alt="">
+    <p><b>Price</b>:${a.products[2].price}</p>
+    <p><b>Description</b>:${a.products[2].description}</p>
+    <br>
+    <a href="">Seller profile</a>
+    <br>
+    <button id="but"><b>Buy</b></button>
+    </div>
+
+    `
+}
