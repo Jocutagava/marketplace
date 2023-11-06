91e00cefe683669f0a8b943d8c5dc6f2306cf2c8
Jocutagava <bratoliubkirill@gmail.com>
2023-11-06 15:02:49 +0100

    fnkfnklqw

--- user1js.js
+++ user1js.js
@@ -1,7 +1,7 @@
 const urlParams = new URLSearchParams(window.location.search);
 const id = urlParams.get("id");
 
-let block = document.getElementById("main-container");
+let block = document.getElementById("karta");
 let productsArray = [];
 let link = 'https://my-json-server.typicode.com/Jocutagava/marketplace';
 let xhr = new XMLHttpRequest();
@@ -12,26 +12,37 @@ xhr.onload = function(){
     let answer = xhr.response;
     block.innerHTML = `
     <div class="visitka">
-        <h1>${answer.name}</h1>
+        <h1 class="visitkName">${answer.name}</h1>
         <h3>${answer.sirname}</h3>
-        <img src="${answer.photo_url}" alt="">
+        <img class="profPicture" src="${answer.photo_url}" alt="">
         <p>Ballance:${answer.ballance}</p>
     </div>
     
  `
 }
 xhr.send();
-/* <div class="seller-products">
+/* */
+
+let prod = document.getElementById("prod")
+let xhm = new XMLHttpRequest();
+xhm.open('GET', `${link}/products?author_id=${id}`);
+xhm.responseType = 'json';
+xhm.onload = function(){ 
+    let antwort = xhm.response;
+    antwort.forEach( a =>{
+        prod.innerHTML += `
+   <div class="seller-products">
         <div class="productOfSeller">
-            <h1><b></b></h1>
-            <img src="" alt="">
-            <p><b></b></p>
-            <p><b></b></p>
+            <h1><b>${a.name}</b></h1>
+            <img src="${a.photo_url}" alt="">
+            <p><b>Price:</b>${a.price}</p>
+            <p><b>Description</b>${a.description}</p>
         </div>
-        <div class="productOfSeller">
-            <h1><b></b></h1>
-            <img src="" alt="">
-            <p><b></b></p>
-            <p><b></b></p>
-        </div>
-    </div> */
+        
+    
+ `
+    })
+    
+}
+
+xhm.send();
